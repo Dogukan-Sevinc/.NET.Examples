91e5a029a9d69bc9e6b514968792877aefb47674
Dogukansvnc <101670829+Dogukansvnc@users.noreply.github.com>
2022-04-19 12:06:56 +0300

    Template - launchSetting

--- AdminTemplate/Program.cs
+++ AdminTemplate/Program.cs
@@ -2,9 +2,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -29,5 +31,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
 
 app.Run();
