036d2faeb03fecd67da652e9d3d910096b6991b7
Dogukansvnc <101670829+Dogukansvnc@users.noreply.github.com>
2022-04-26 11:49:24 +0300

    FrontEnd-Update

--- FrontEnd/JavaScript/2.js
+++ FrontEnd/JavaScript/2.js
@@ -1,61 +1,60 @@
-console.log("2 ok")
+console.log("2 ok");
 
-//operatörler
-//Matematiksel oparatörler
+//Operatörler
+//Matematiksel operatörler
 // + - * / %
 // ++ -- ** 
-// += -= *= /= %= 
-// Mantıksal operatörler
-// < > <= == ! != === !== && || 
+//+= -= *= /= %=
+//Mantıksal operatörler
+// < > <= <= == ! != === !== && || 
 
-function kontrol(){
-     var a =10 ; 
-     var b= "10";
-     console.log("a=" + typeof a);
-     console.log("b="+typeof b);
-    if(a==b & typeof a== typeof b){
-        //=== çalışma mantığı 
+function kontrol() {
+    var a = 10; 
+    var b="10";
+    console.log("a = " + typeof a);
+    console.log("b = " + typeof b);
+    if (a == b & typeof a == typeof b) {
+        // === çalışma mantığı
         console.log("a=b");
+    } else {
+        console.log("a!=b");
     }
-    else{
-        console.log("a!=b")
-    } 
-    console.log(a/0);
-
+    console.log( a / 0)
 }
 
-function faktoriyel (n) {
-    var sonuc = 1;
-    for(var i=1; i<=n; i++) {
-    sonuc *=i;
+//NaN (Not a Number)
+//Infinity (Sonsuz)
+
+function faktoriyel(n) {
+    var sonuc =1;
+    for (var i =1; i<= n; i++){
+        sonuc *=i;
     }
     return sonuc;
+}
 
-    var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    function diziDongu()
-    {
-        Console.log("for")
-        for (var i = 0; i < array.Length; i++){
-            var item = array[i];
-            Console.log(item);
-        }
-        Console.log("forEach")
-        array.forEach(item=> {
-            console.log(item);
-        });
-        console.log("map")
-        array.map(item=> {
-            console.log(item)
-        });
+var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function diziDongu() {
+    console.log("for")
+    for (var i =0; i < array.length; i++) {
+        var item = array[i];
+        console.log(item);
     }
+    console.log("forEach")
+    array.forEach(item => {
+        console.log(item);
+    });
+    console.log("map")
+    array.map(item => {
+        console.log(item);
+    });
+}
 
-    //arrow function
-    var diziDongu2 = () => {
-        
-        array.map( item=> {
-            array.map(item=> {
-                console.log(item);
-        });
-        
-    }
+//arrow function 
+var diziDongu2 = () => {
+    array.map((item,index) => {
+    console.log("Index: "+index + " Değer: " + item);
+    console.log("itself: " + itself);
+});
+
 }
