c71dc26f7efec02b91fdc69cc78cd44db257ec0b
Dogukansvnc <101670829+Dogukansvnc@users.noreply.github.com>
2022-04-28 10:24:30 +0300

    FrontEnd-JavaScript

--- FrontEnd/JavaScript/4.js
+++ FrontEnd/JavaScript/4.js
@@ -2,14 +2,20 @@ console.log("4.js dosyası çalıştı")
 
 //GeoLocation
 
-var getLocation = () => {
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPostion(showPosition);
-    }else{
+var initMap = () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
         console.log("Geolocation is not supported by this browser.");
     }
-
+}
 var showPosition = (position) => {
     console.log(position);
 }
-}
+
+
+// latitude: 41.844008982786345
+// longitude: 29.007230753851537
+
+
+
