31cbdb2bcc5fe7106272fba480168d3c6a19cbee
Dogukansvnc <101670829+Dogukansvnc@users.noreply.github.com>
2022-04-28 14:06:00 +0300

    GoogleMapApp

--- FrontEnd/JavaScript/5.js
+++ FrontEnd/JavaScript/5.js
@@ -14,43 +14,59 @@ var showPosition = (position) => {
         lat: position.coords.latitude,
         lng: position.coords.longitude
     };
-    console.log(pos);
-    var mapDiv = document.getElementById('map');
-    var map = new google.maps.Map(mapDiv, {
+
+    const map = new google.maps.Map(document.getElementById("map"), {
         center: pos,
         zoom: 18,
-        mapTypeId: "terrain",
-        mapTypeControl: false
+        mapTypeControl: false,
+    });
+    const card = document.getElementById("pac-card");
+    const input = document.getElementById("pac-input");
+    const options = {
+        fields: ["formatted_address", "geometry", "name"],
+        strictBounds: false,
+        types: ["establishment"],
+    };
+
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(card);
+
+    const autocomplete = new google.maps.places.Autocomplete(input, options);
+    autocomplete.bindTo("bounds", map);
+
+    const infowindow = new google.maps.InfoWindow();
+    const infowindowContent = document.getElementById("infowindow-content");
+
+    infowindow.setContent(infowindowContent);
+
+    const marker = new google.maps.Marker({
+        map,
+        anchorPoint: new google.maps.Point(0, -29),
     });
 
-    }
+    autocomplete.addListener("place_changed", () => {
+        infowindow.close();
+        marker.setVisible(false);
 
-    var marker = new google.maps.Marker({
-        position: pos,
-        map: map
+        const place = autocomplete.getPlace();
+        console.log(place);
+        if (!place.geometry || !place.geometry.location) {
+            window.alert("No details available for input: '" + place.name + "'");
+            return;
+        }
+
+        // If the place has a geometry, then present it on a map.
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);
+        }
+
+        marker.setPosition(place.geometry.location);
+        marker.setVisible(true);
+        infowindowContent.children["place-name"].textContent = place.name;
+        infowindowContent.children["place-address"].textContent =
+            place.formatted_address;
+        infowindow.open(map, marker);
     });
-
-    map.addListener('click', (e) => {
-        var posClick = {
-            lat: e.latLng.lat(),
-            lng: e.latLng.lng()
-        };
-        //markerları temizle
-        marker.setMap(null);
-        //tıklanan konuma göre marker ekle
-        marker = new google.maps.Marker({
-            position: posClick,
-            map: map,
-            title: 'Clicked Location',
-            animation: google.maps.Animation.DROP
-        });
-    } );
 }
-
-// latitude: 41.044008982786345
-// longitude: 29.007230753851537
-
-//https://www.google.com/maps/@41.04400551057116,29.007235110826237,20z
-
-//latitude: 41.04400551057116
-//longitude: 29.007235110826237
