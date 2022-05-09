1d287f9868f4fba6b27d1ac54ccefb3654d7f963
Dogukansvnc <101670829+Dogukansvnc@users.noreply.github.com>
2022-05-09 10:37:47 +0300

    Entities

--- AdminTemplate/Models/Entities/Category.cs
+++ AdminTemplate/Models/Entities/Category.cs
@@ -4,7 +4,7 @@ namespace AdminTemplate.Models.Entities;
 
 public class Category : BaseEntity<int>
 {
-    public string CategoryName { get; set; }
+    public string Name { get; set; }
     public string Description { get; set; }
 
     public IList<Product>? Products { get; set; }
--- AdminTemplate/Models/Entities/Product.cs
+++ AdminTemplate/Models/Entities/Product.cs
@@ -5,7 +5,7 @@ namespace AdminTemplate.Models.Entities;
 public class Product : BaseEntity<Guid>
 {
     public string Name { get; set; }
-    public decimal UnitPrice { get; set; }
+    public decimal UnitPrice { get; set; } = 0;
     public int CategoryId { get; set; }
 
     public Category Category { get; set; }
