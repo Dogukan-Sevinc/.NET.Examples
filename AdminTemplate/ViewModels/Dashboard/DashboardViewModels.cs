b9386ca661e97416bb086166ee93508c758c71f0
Dogukansvnc <101670829+Dogukansvnc@users.noreply.github.com>
2022-05-09 10:37:34 +0300

    Dashboard

---
+++ AdminTemplate/ViewModels/Dashboard/DashboardViewModels.cs
@@ -0,0 +1,9 @@
+﻿using AdminTemplate.Controllers;
+
+namespace AdminTemplate.ViewModels.Dashboard
+{
+    public class DashboardViewModels
+    {
+        public ProductReportViewModel? ProductReportViewModel { get; set; }
+    }
+}
