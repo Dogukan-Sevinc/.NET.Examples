2ffc6de9fd1e60b95bd8d1e6352524af3f2560bc
Dogukansvnc <101670829+Dogukansvnc@users.noreply.github.com>
2022-06-08 10:44:41 +0300

    North.Business ok

--- North.Business/Repositories/ProductRepo.cs
+++ North.Business/Repositories/ProductRepo.cs
@@ -1,6 +1,7 @@
 ﻿using North.Businesss.Repositories.Abstracts.EntityFrameworkCore;
 using North.Core.Entities;
 using North.Data;
+using System.Linq.Expressions;
 
 namespace North.Businesss.Repositories;
 
@@ -8,5 +9,12 @@ public class ProductRepo : RepositoryBase<Product, int>
 {
     public ProductRepo(NorthwindContext context) : base(context)
     {
+    }
+
+    public override IQueryable<Product> Get(Expression<Func<Product, bool>> predicate = null)
+    {
+        if (predicate == null)
+            return _table.Where(x => !x.Discontinued);
+        return base.Get(predicate);
     }
 }
